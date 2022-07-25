9dc2ebb5ce9bfe288db7c95d7d143f0b45e62d44
axelvega <caaxgave@gmail.com>
2022-07-25 11:39:40 -0500

    Cambio hoy

--- train_network.py
+++ train_network.py
@@ -16,6 +16,7 @@ from losses.discriminator_loss import DiscriminatorLoss
 from losses.discriminator_loss import adversarial_loss
 from evaluate import evaluate
 
+#### prueba
 
 def train_net(net,
               net_D,
