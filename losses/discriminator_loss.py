664bfa0310088e6a6a7bbdad7791028d4d7d627e
axelvega <caaxgave@gmail.com>
2022-08-01 17:43:14 -0500

    change: using C and W constants and L1Loss with reduction=sum, bilinear=False

--- losses/discriminator_loss.py
+++ losses/discriminator_loss.py
@@ -12,8 +12,8 @@ class DiscriminatorLoss(nn.Module):
     def forward(self, t, y):
         # ps = t.size(dim=2)
         #epsilon = torch.tensor([[10e-09]], requires_grad=False).to(device=self.device, dtype=torch.float32)
-        loss_real = -torch.mean(torch.log(torch.sigmoid(self.net_d(t) + 1e-9)))
-        loss_generated = -torch.mean(torch.log(torch.sigmoid(1 - self.net_d(y.detach()) + 1e-9)))
+        loss_real = -torch.mean(torch.log(torch.sigmoid(self.net_d(t)) + 1e-9))
+        loss_generated = -torch.mean(torch.log(1 - torch.sigmoid(self.net_d(y.detach())) + 1e-9))
         # disc_loss = loss_real + loss_generated
 
         return loss_real, loss_generated
@@ -22,7 +22,7 @@ class DiscriminatorLoss(nn.Module):
 def adversarial_loss(net_d, y, device):
     ps = y.size(dim=2)
     #epsilon = torch.tensor([[10e-09]], requires_grad=False).to(device=device, dtype=torch.float32)
-    adv_loss = -torch.mean(torch.log(torch.sigmoid(net_d(y) + 1e-9)))
+    adv_loss = -torch.mean(torch.log(torch.sigmoid(net_d(y)) + 1e-9))
     # adv_loss = (12*ps*ps)*torch.mean(adv_loss)
 
     return adv_loss
