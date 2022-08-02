9aeb9502a13af02ba44e601b7aeb566525342b0c
axelvega <caaxgave@gmail.com>
2022-08-02 14:41:35 -0500

    back to my implementation

--- train_network.py
+++ train_network.py
@@ -11,14 +11,12 @@ from tqdm import tqdm
 import csv
 import pandas as pd
 import torchvision.transforms as T
-import torch.nn.functional as F
 from utils.data_loading import ImageDataset
 from utils.pyramids import GaussianPyramid
 from losses.discriminator_loss import DiscriminatorLoss
 from losses.discriminator_loss import adversarial_loss
 from evaluate import evaluate
 from contextlib import contextmanager
-from losses.losses import GeneratorLoss, DiscLoss
 import warnings
 
 
@@ -82,9 +80,6 @@ def train_net(net,
     global_step = 0
     dict_losses_list = []
 
-    discriminator_loss = DiscLoss()
-    gen_loss = GeneratorLoss(size=ps)
-
     # 5. Begin training
     for epoch in range(epochs):
 
@@ -113,96 +108,79 @@ def train_net(net,
                 for pyramid in G_pyramid:
                     G_pyramid[pyramid] = G_pyramid[pyramid].to(device=device, dtype=torch.float32)
 
-                laplacian_pyr, y_pred = net(exp_images)
-                y_pred = [y for y in y_pred.values()]
-                #mae_loss = nn.L1Loss(reduction='sum')
-                #bcelog_loss = nn.BCEWithLogitsLoss()
-                g_pyramid = [t for t in G_pyramid.values()]
+                with torch.cuda.amp.autocast(enabled=amp):
 
-                if (epoch+1 >= 15) and (ps == 256):
+                    laplacian_pyr, y_pred = net(exp_images)
 
-                    # Adversarial Loss (only for 256 patches
-                    #_, y_pred_2 = net(exp_images)
-                    y_pred_2 = [Y.detach() for Y in y_pred]
-                    disc_fake = net_D(y_pred_2[-1])
-                    #disc_fake = net_D(y_pred['subnet_16'].detach())
-                    #fake_loss = bcelog_loss(disc_fake, torch.zeros_like(disc_fake))
-                    disc_real = net_D(g_pyramid[-1])
-                    #real_loss = bcelog_loss(disc_real, torch.ones_like(disc_real))
-                    disc_loss = discriminator_loss(disc_fake, disc_real)
+                    # Critertions for Losses:
+                    mae_loss = nn.L1Loss()
+                    bcelog_loss = nn.BCEWithLogitsLoss()   # This already includes sigmoid
+
+                    if (epoch+1 >= 15) and (ps == 256):
+
+                        # Adversarial Loss (only for 256 patches
+                        #_, y_pred = net(exp_images)
+                        y_pred_2 = [Y.detach() for Y in y_pred.values()]
+                        disc_fake = net_D(y_pred_2[-1])
+                        fake_loss = bcelog_loss(disc_fake, torch.zeros_like(disc_fake))
+                        disc_real = net_D(G_pyramid['level1'])
+                        real_loss = bcelog_loss(disc_real, torch.ones_like(disc_real))
+                        disc_loss = (fake_loss + real_loss) #/2
+
+                        # DISCRIMINATOR TRAINING
+                        d_optimizer.zero_grad()
+                        disc_loss.backward(retain_graph=True)
+                        d_optimizer.step()
+
+                        disc_adv = net_D(y_pred['subnet_16'])
+                        adv_loss = bcelog_loss(disc_adv, torch.ones_like(disc_adv))
+
+                        #adv_loss = adversarial_loss(net_D, y_pred['subnet_16'], device=device)
+
+                    else:
+                        disc_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
+                        real_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
+                        fake_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
+                        adv_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
+
+                    # Generator Loss
+                    loss_generator = 4 * mae_loss(y_pred['subnet_24_1'],
+                                                  F.interpolate(G_pyramid['level4'], (y_pred['subnet_24_1'].shape[2],
+                                                                                      y_pred['subnet_24_1'].shape[3]),
+                                                                mode='bilinear', align_corners='True')) + \
+                    2 * mae_loss(y_pred['subnet_24_2'],
+                                 F.interpolate(G_pyramid['level3'], (y_pred['subnet_24_2'].shape[2],
+                                                                     y_pred['subnet_24_2'].shape[3]),
+                                               mode='bilinear', align_corners='True')) + \
+                    mae_loss(y_pred['subnet_24_3'],
+                             F.interpolate(G_pyramid['level2'], (y_pred['subnet_24_3'].shape[2],
+                                                                 y_pred['subnet_24_3'].shape[3]),
+                                           mode='bilinear', align_corners='True')) + \
+                    mae_loss(y_pred['subnet_16'], G_pyramid['level1']) + adv_loss
 
 
-                    #DL = DiscriminatorLoss(net_d=net_D, device=device)
-                    #real_loss, fake_loss = DL(G_pyramid['level1'], y_pred['subnet_16'])
-                    #disc_loss = (fake_loss + real_loss) #/ 2
-
-                    # DISCRIMINATOR TRAINING
-                    d_optimizer.zero_grad()
-                    disc_loss.backward(retain_graph=True)
-                    d_optimizer.step()
-
-                    #disc_adv = net_D(y_pred['subnet_16'])
-                    #adv_loss = bcelog_loss(disc_adv, torch.ones_like(disc_adv))
-                    #adv_loss = adversarial_loss(net_D, y_pred['subnet_16'], device)
-                    disc_fake = net_D(y_pred[-1])
-                    rec_loss, pyr_loss, adv_loss, loss_generator = gen_loss(y_pred, g_pyramid, disc_fake, withoutadvloss=False)
-
-
-                else:
-
-                    disc_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
-                    #real_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
-                    #fake_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
-                    adv_loss = torch.tensor([[0]]).to(device=device, dtype=torch.float32)
-                    rec_loss, pyr_loss, loss_generator = gen_loss(y_pred, g_pyramid, withoutadvloss=True)
-
-
-                # Generator Loss
-                # C = (ps**2)* 3
-                # W = (ps ** 2) * 12
-                # loss_generator = (1/C)*((4 * mae_loss(y_pred['subnet_24_1'],
-                #                               F.interpolate(G_pyramid['level4'],
-                #                                             (y_pred['subnet_24_1'].shape[2],
-                #                                              y_pred['subnet_24_1'].shape[3]))) +
-                #                  2 * mae_loss(y_pred['subnet_24_2'],
-                #                               F.interpolate(G_pyramid['level3'],
-                #                                             (y_pred['subnet_24_2'].shape[2],
-                #                                              y_pred['subnet_24_2'].shape[3]))) +
-                #                  mae_loss(y_pred['subnet_24_3'], F.interpolate(G_pyramid['level2'],
-                #                                             (y_pred['subnet_24_3'].shape[2],
-                #                                              y_pred['subnet_24_3'].shape[3]))) +
-                #                  mae_loss(y_pred['subnet_16'], F.interpolate(G_pyramid['level1'],
-                #                                             (y_pred['subnet_16'].shape[2],
-                #                                              y_pred['subnet_16'].shape[3]))))/y_pred['subnet_16'].shape[0] \
-                #                  + W*adv_loss)
-
-                #GENERATOR TRAINING
+                # GENERATOR TRAINING
                 g_optimizer.zero_grad()
                 loss_generator.backward()
                 g_optimizer.step()
 
                 pbar.update(exp_images.shape[0])
                 global_step += 1
-
+                #epoch_loss += final_loss.item()
                 epoch_loss += loss_generator.item()
 
-                # if global_step % 50 == 0:
-                #     train_report = {'epoch': epoch+1, 'step': global_step, 'Generator loss': loss_generator.item(),
-                #                     'Discriminator loss': disc_loss.item(), 'Real loss': real_loss.item(),
-                #                     'Fake loss': fake_loss.item(), 'lr': g_optimizer.param_groups[0]['lr']}
-                #     dict_losses_list.append(train_report)
                 if global_step % 50 == 0:
                     train_report = {'epoch': epoch+1, 'step': global_step, 'Generator loss': loss_generator.item(),
-                                    'Discriminator loss': disc_loss.item(), 'Pyr loss': pyr_loss.item(),
-                                    'Rec loss': rec_loss.item(), 'lr': g_optimizer.param_groups[0]['lr']}
+                                    'Discriminator loss': disc_loss.item(), 'Real loss': real_loss.item(),
+                                    'Fake loss': fake_loss.item(), 'lr': g_optimizer.param_groups[0]['lr']}
                     dict_losses_list.append(train_report)
 
                 # LOSSES LOGS
                 experiment.log({
                     #'train loss': final_loss.item(),
                     'Generator loss (batch)': loss_generator.item(),
-                    #'Real loss (batch)': real_loss.item(),
-                    #'Fake loss (batch)': fake_loss.item(),
+                    'Real loss (batch)': real_loss.item(),
+                    'Fake loss (batch)': fake_loss.item(),
                     'Discriminator loss (batch)': disc_loss.item(),
                     'Adversarial loss (batch)': adv_loss.item(),
                     'step': global_step,
@@ -231,8 +209,8 @@ def train_net(net,
 
                 pbar.set_postfix(**{#'train loss': final_loss.item(),
                                     'Generator loss (batch)': loss_generator.item(),
-                                    #'Real loss (batch)': real_loss.item(),
-                                    #'Fake loss (batch)': fake_loss.item(),
+                                    'Real loss (batch)': real_loss.item(),
+                                    'Fake loss (batch)': fake_loss.item(),
                                     'Discriminator loss (batch)': disc_loss.item(),
                                     'Adversarial loss (batch)': adv_loss.item()})
 
