060fb32251a65627f9ffbcaf13f020f787bacc06
axelvega <caaxgave@gmail.com>
2022-08-03 13:46:16 -0500

    change: weights initialize

--- generator.py
+++ generator.py
@@ -46,8 +46,12 @@ class Generator(nn.Module):
 
     def initialize_weights(self):
         for m in self.modules():
-            if isinstance(m, (nn.Conv2d, nn.LeakyReLU)):
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
                 nn.init.kaiming_normal_(m.weight)
 
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0.0)
+
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0.0)
