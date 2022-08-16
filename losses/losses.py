4edec5075ba81b58899b242196190e3f9a1ec414
axelvega <caaxgave@gmail.com>
2022-08-16 16:17:48 -0500

    change: using SSIMLoss with pad=1 in AvrPool, last level on Pyr, no weighting

--- losses/losses.py
+++ losses/losses.py
@@ -35,10 +35,11 @@ class SSIMLoss(nn.Module):
 
         SSIM_n = (2 * mu_x * mu_y + self.C1) * (2 * sigma_xy + self.C2)
         SSIM_d = (mu_x ** 2 + mu_y ** 2 + self.C1) * (sigma_x + sigma_y + self.C2)
-        print(SSIM_n.size())
-        print(SSIM_d.size())
+        output = torch.clamp((1 - SSIM_n / SSIM_d) / 2, 0, 1)
+        print(output.size())
+        print(output)
 
-        return torch.clamp((1 - SSIM_n / SSIM_d) / 2, 0, 1)
+        return output
 
 
 class PyrLoss (nn.Module):
